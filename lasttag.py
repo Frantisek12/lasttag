897d015f1eb6443b91d4c061374af680de2555d7
frantisek <frantisek.komarek@partners.cz>
2021-04-29 13:28:59 +0200

    use current dir when path not specified

--- lasttag.py
+++ lasttag.py
@@ -1,13 +1,15 @@
 #!/bin/python
 
+import os
 import sys
 import git
 
 if len(sys.argv) == 1:
-    print('Path is required')
-    exit()
+    print('Path is required, using current path')
+    wd = os.getcwd()
+else:
+    wd = sys.argv[1]
 
-wd = sys.argv[1]
 repo = git.Repo(wd)
 
 tags = sorted(repo.tags, key=lambda t: t.commit.committed_datetime)
